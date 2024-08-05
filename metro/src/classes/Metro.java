34f01e38b8bef69557c72aba2cce82d440721bef
Helia Akhtarkavian <heliakaviansharif@gmail.com>
2024-08-05 19:28:19 +0330

    feat: add primary Station methods

--- metro/src/classes/Metro.java
+++ metro/src/classes/Metro.java
@@ -19,6 +19,14 @@ public class Metro {
             this.name = name;
         }
 
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 
 
