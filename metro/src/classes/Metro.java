654e1f924c61ef69dab5f83bc6552ad42bb1d41e
Helia Akhtarkavian <heliakaviansharif@gmail.com>
2024-08-06 14:53:10 +0330

    feat:add some necessary override methods to Station

--- metro/src/classes/Metro.java
+++ metro/src/classes/Metro.java
@@ -107,6 +107,19 @@ public class Metro {
         public String toString() {
             return name;
         }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Station station = (Station) obj;
+            return Objects.equals(name, station.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
     }
 
 
